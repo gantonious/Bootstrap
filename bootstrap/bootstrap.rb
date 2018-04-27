6f9289f61d6ac7c16dc51370b21f9a6548462070
George Antonious <george@antonious.ca>
2018-04-27 00:49:36 -0600

    Renaming

--- bootstrap/bootstrap.rb
+++ bootstrap/bootstrap.rb
@@ -1,6 +1,6 @@
 require_relative "core/Environment"
 require_relative "core/Pipeline"
-require_relative "plugins/homebrew/bootstrap.rb"
+require_relative "plugins/homebrew/brew_bootstraper.rb"
 
 Environment.use_path "./environment"
 
