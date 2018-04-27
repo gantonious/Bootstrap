937c745cfce7f150518f09cce0856e0876e007b2
George Antonious <george@antonious.ca>
2018-04-26 23:16:12 -0600

    Start working on bootstrap

---
+++ bootstrap/plugins/homebrew/brew.rb
@@ -0,0 +1,35 @@
+module Brew
+    def self.installed?(name)
+        Shell.run_silently "brew list | grep -w #{name}"
+    end
+
+    def self.not_installed?(name)
+        !self.installed?(name)
+    end
+
+    def self.install(name)
+        Shell.run "brew install #{name}"
+    end
+
+    def self.uninstall(name)
+        Shell.run "brew uninstall #{name}"
+    end
+
+    module Cask
+        def self.installed?(cask_name)
+            Shell.run_silently "brew cask list | grep -w #{cask_name}"
+        end
+    
+        def self.not_installed?(cask_name)
+            !self.installed?(cask_name)
+        end
+
+        def self.install(cask_name)
+            Shell.run "brew cask install #{cask_name}"
+        end
+
+        def self.uninstall(cask_name)
+            Shell.run "brew cask uninstall #{cask_name}"
+        end
+    end
+end
